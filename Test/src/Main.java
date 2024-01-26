a00c910a83e8031434529d5772088c4a990373d3
joshua <joshua.ekaputra@gmail.com>
2024-01-26 11:34:43 +0100

    add Parser

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,4 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,23 +13,22 @@ public class Main {
 
 
     public static void main(String[] args) {
-
-
-
+        Parser parser = new Parser();
+        System.out.println();
 
     }
 
     class Person {
 
-        public String fistname;
+        public String firstname;
         public String lastname;
 
         public int hospitalId;
 
         public List<Address> addressList;
 
-        public Person(String fistname, String lastname, List<Address> addressList, int hospitalId) {
-            this.fistname = fistname;
+        public Person(String firstname, String lastname, List<Address> addressList, int hospitalId) {
+            this.firstname = firstname;
             this.lastname = lastname;
             this.addressList = addressList;
             this.hospitalId = hospitalId;
@@ -82,6 +86,57 @@ public class Main {
             this.providerId = providerId;
             this.address = address;
             this.treatments = new ArrayList<>();
+        }
+    }
+
+    class Parser {
+        public Parser() {
+
+        }
+
+        public String read_csv(String file_address) throws IOException {
+            BufferedReader br = new BufferedReader(new FileReader(file_address));
+            try {
+
+                StringBuilder sb = new StringBuilder();
+                String line = br.readLine();
+
+                while (line != null) {
+                    sb.append(line);
+                    sb.append(System.lineSeparator());
+                    line = br.readLine();
+                }
+                String everyline = sb.toString();
+                return everyline;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                br.close();
+            }
+        }
+
+        public List<Address> stringToAddresses(String addressList) {
+            String noBracket = addressList.replace("[", "").replace("]", "");
+            String[] parts = noBracket.split("|");
+            int id = Integer.parseInt(parts[0]);
+            String state = parts[1];
+            String city = parts[2];
+            String street = parts[3];
+            int houseNumber = Integer.parseInt(parts[4]);
+            int zipCode = Integer.parseInt(parts[5]);
+            String refRegion = parts[6];
+
+            return List.of(new Address[]{new Address(id, state, city, street, houseNumber, zipCode, refRegion)});
+        }
+
+        public List<Person> csvToPersons(String csv) {
+            List<Person> ret = new ArrayList<>();
+            for(String description: csv.split("\n")) {
+                String[] parts = description.split(";");
+                Person cur = new Person(parts[0], parts[1], this.stringToAddresses(parts[2]), Integer.parseInt(parts[3]));
+                ret.add(cur);
+            }
+            return ret;
         }
     }
 }
